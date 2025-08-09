9a1359190cf16da9199be3f0e51ca913f2683be7
shohata <so.shohata@gmail.com>
2025-08-09 11:49:12 +0900

    config cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,11 +4,16 @@ export default defineConfig({
   e2e: {
     // テスト対象のアプリケーションのベースURLを設定
     baseUrl: "http://localhost:3000",
-    // supportFileをfalseに設定して、e2e.tsファイルを探さないように設定
-    supportFile: false,
     // Node.jsのイベントリスナーをここで設定
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
   },
 });
